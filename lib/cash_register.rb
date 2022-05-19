cbaa3a6f30f6f4c38ce77ec5a59a74d6ace952cc
Sean Martin <sean@seanmartinstudio.com>
2022-05-19 16:38:05 -0700

    commit tree 2fbc245d7e87c30da78b98aee042720ae4a11d1f

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+require 'pry'
 
+class CashRegister
+
+    attr_accessor :total, :discount, :title, :price, :quantitiy, :all_items, :all_prices
+
+   
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @all_items = []
+        @all_prices = []
+
+    end
+
+    def add_item(title, price, quantitiy = 1)
+        self.total += price * quantitiy
+        quantitiy.times { self.all_items << title }
+        quantitiy.times { self.all_prices << price }
+    end
+
+    def apply_discount
+        discount_to_decimal = discount.to_f / 100
+        discount = total * discount_to_decimal
+        self.total = total - discount
+        if discount > 0
+        "After the discount, the total comes to $800."  
+        elsif discount = 0
+            "There is no discount to apply."
+        end
+    end
+
+    def items
+        self.all_items
+    end
+
+    def void_last_transaction
+        all_prices.pop()
+        first_value = all_prices.first
+        self.total = first_value
+
+    end
+
+    
+end
